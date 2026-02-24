0b9d4910964cc65fce7d48223ec17b518102e884
JuanPerez27 <juan.perez.cruz@correounivalle.edu.co>
2026-02-23 20:45:06 -0500

    Cambio del localHost a 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -40,5 +40,5 @@ app.use(errorHandler);
 
 const PORT = Number(process.env.PORT || 5000);
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
